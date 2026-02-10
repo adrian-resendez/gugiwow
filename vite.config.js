2b7189aff30345a00ea3eacadce909eb5ffcf7fb
adrian-resendez <adrian-resendez@outlook.com>
2026-02-09 19:51:51 -0800

    fixed base

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'docs'
-  },
-  base: '/gugiwow/'
+  base: '/gugiwow/', // 🔴 REQUIRED for GitHub Pages
 })
